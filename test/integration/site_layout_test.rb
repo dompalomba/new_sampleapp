22971ee607262faddb46824b7f10e91abc0599a5
Domenico <dompalomba@gmail.com>
2015-02-07 22:10:02 +0000

    5.3.2 link tests@

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+    test "layout links" do
+        get root_path
+        assert_template 'static_page/home'
+        assert_select "a[href=?]", root_url, count:2
+        assert_select "a[href=?]", help_url
+        assert_select "a[href=?]", about_url
+        assert_select "a[href=?]", contact_url
+    end
+
+end

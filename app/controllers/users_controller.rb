28da273496dea2dada999c6b97af2457d61e8bd6
Domenico <dompalomba@gmail.com>
2015-02-23 21:52:16 +0000

    7.2.1 Using form for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,11 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-    
+    def create
+        @user = User.new(params[:user])
+        if @user.save
+            #save is successful
+        else
+            render 'new'
+        end 
 end
